19e1537c5c5e5922d12ca3d71c90b23e09e14af3
MengYaoLi <54579442+hero233-li@users.noreply.github.com>
2026-07-31 17:47:41 +0800

    refactor: remove obsolete HTTP application-link endpoints

--- Alkaid-python/apps/integrations/cjdk_jyrc/api.py
+++ Alkaid-python/apps/integrations/cjdk_jyrc/api.py
@@ -2,28 +2,9 @@ from apps.integrations.contracts import EndpointSpec, RetryMode
 from apps.integrations.cjdk_jyrc.models import (
     AgreementDocumentEnvelope,
     AgreementPreviewEnvelope,
-    GenerateApplicationLinkEnvelope,
     QueryAgreementEnvelope,
 )
 
-
-CREATE_SUN_CODE_LINK = EndpointSpec(
-    operation_id="cjdk_jyrc.generate_sun_code_link",
-    method="POST",
-    path="/links/sun-code",
-    response_model=GenerateApplicationLinkEnvelope,
-    success_path="code",
-    success_values=("0000",),
-)
-
-CREATE_DYNAMIC_LINK = EndpointSpec(
-    operation_id="cjdk_jyrc.generate_dynamic_link",
-    method="POST",
-    path="/links/dynamic",
-    response_model=GenerateApplicationLinkEnvelope,
-    success_path="code",
-    success_values=("0000",),
-)
 
 QUERY_AGREEMENT_TEMPLATES = EndpointSpec(
     operation_id="cjdk_jyrc.query_agreement_templates",
